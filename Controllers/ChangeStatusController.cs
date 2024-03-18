5446955850c8167354c1a1428d35932d4b869641
CricketApplication06 <cricketapplication06@gmail.com>
2024-03-18 10:33:32 +0530

    ChangeStatus module design

---
+++ Controllers/ChangeStatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cricket_Application.Controllers
+{
+    public class ChangeStatusController : Controller
+    {
+        public IActionResult ChangeStatus()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ChangeStatusModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Cricket_Application.Models
+{
+    public class ChangeStatusModel
+    {
+        public int MatchID { get; set; }
+        public string TeamName { get; set; }
+        public string TeamLogo { get; set; }
+        public string MatchStatus { get; set; }
+
+    }
+}
