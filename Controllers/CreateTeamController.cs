33591133b0f25c8e13b71ee1dd9a9d9d20863cf0
CricketApplication06 <cricketapplication06@gmail.com>
2024-03-19 15:45:42 +0530

    CreateTeamDBHandler Updated

--- Controllers/CreateTeamController.cs
+++ Controllers/CreateTeamController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cricket_Application.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cricket_Application.Controllers
 {
     public class CreateTeamController : Controller
     {
-        public IActionResult CreateTeam()
+        public IActionResult CreateTeam(CreateTeam createTeam)
         {
+            if (ModelState.IsValid)
+            {
+                CreateTeamDBHandler ihandler = new CreateTeamDBHandler();
+                if (ihandler.insertCreateTeam(createTeam))
+                {
+                    ViewBag.message = "Record Inserted Successfully";
+                    ModelState.Clear();
+                }
+            }
             return View();
         }
-        
+
     }
 }
