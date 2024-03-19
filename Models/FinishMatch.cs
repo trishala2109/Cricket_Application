7445c4ddc0b5e2dfc0f0a86423d4b51539ea551a
sayalikapse30 <sayalikapse53@gmail.com>
2024-03-19 11:35:26 +0530

    criketApplication

---
+++ Models/FinishMatch.cs
@@ -0,0 +1,14 @@
+﻿namespace Cricket_Application.Models
+{
+    public class FinishMatch
+    {
+        public int MatchId { get; set; }
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public string TeamLogo { get; set; }
+        public string WinningTeam {  get; set; }
+
+
+
+    }
+}
