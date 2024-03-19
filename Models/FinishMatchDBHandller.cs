6468cbce7405d1322063f9e380c37c13b52db346
sayalikapse30 <sayalikapse53@gmail.com>
2024-03-19 16:55:27 +0530

    Finish_match_update

---
+++ Models/FinishMatchDBHandller.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+namespace Cricket_Application.Models
+{
+    public class FinishMatchDBHandller
+    {
+        SqlConnection con;
+        private void connection()
+        {
+            string constring = "server=.\\sqlexpress;integrated security=true;database=Cricket_Application";
+            con=new SqlConnection(constring);
+
+        }
+
+       
+        
+
+    }
+}
